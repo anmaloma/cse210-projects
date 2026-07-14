3410a24f4c6952a16919a5faf6ded226d53e1b6d
anmaloma <proyectoayuda22@gmail.com>
2026-07-13 18:01:19 -0600

    Journalw2

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -4,6 +4,77 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Journal journal = new Journal();
+        PromptGenerator promptGenerator = new PromptGenerator();
+
+        int choice = 0;
+
+        while (choice != 5)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Journal Menu");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("Select a choice: ");
+
+            choice = int.Parse(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                string prompt = promptGenerator.GetRandomPrompt();
+
+                Console.WriteLine();
+                Console.WriteLine(prompt);
+                Console.Write("> ");
+
+                string answer = Console.ReadLine();
+
+                Entry entry = new Entry();
+                entry._date = DateTime.Now.ToShortDateString();
+                entry._promptText = prompt;
+                entry._entryText = answer;
+
+                journal.AddEntry(entry);
+
+                Console.WriteLine("Entry added!");
+            }
+            else if (choice == 2)
+            {
+                Console.WriteLine();
+                journal.DisplayAll();
+            }
+            else if (choice == 3)
+            {
+                Console.Write("Filename: ");
+                string file = Console.ReadLine();
+
+                if (System.IO.File.Exists(file))
+                {
+                    journal.LoadFromFile(file);
+                }
+                else
+                {
+                    Console.WriteLine("File not found.");
+                }
+            }
+            else if (choice == 4)
+            {
+                Console.Write("Filename: ");
+                string file = Console.ReadLine();
+
+                journal.SaveToFile(file);
+            }
+            else if (choice == 5)
+            {
+                Console.WriteLine("Goodbye!");
+            }
+            else
+            {
+                Console.WriteLine("Invalid option.");
+            }
+        }
     }
 }
